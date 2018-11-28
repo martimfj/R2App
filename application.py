ebc67699e9cd318c5bd7fa7558894f54a293a0e0
Martim <martimfj@al.insper.edu.br>
2018-11-28 13:29:28 -0200

    Docker tests

--- application.py
+++ application.py
@@ -113,4 +113,4 @@ def main():
     return render_template('index.html', rooms=output, process=process, hostname=hostname, error_message=error_message)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port="5000")
